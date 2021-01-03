eee4b1729f9712e8e0fb7e405348ff03154be187
Zheng duanyang <you@example.com>
2021-01-02 19:11:53 -0500

    01/02/2021_3

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
-#from send_email import send_email
+# from send_email import send_email
 from sqlalchemy.sql import func
 #from werkzeug import secure_filename
 
@@ -8,19 +8,19 @@ from sqlalchemy.sql import func
 app=Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:sm00th1y!@localhost/order_collector'
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://lgigskweroooiu:1e48d154ed70f7ebfc32962330bcf96940fbccfe931fb8697870bc52ba0d82b7@ec2-52-204-20-42.compute-1.amazonaws.com:5432/da9uvl2gj8vicn?sslmode=require'
-db=SQLAlchemy(app)
+# db=SQLAlchemy(app)
 
-class Data(db.Model):
-    __tablename__="data"
-    id =db.Column(db.Integer, primary_key=True)
-    email_=db.Column(db.String(120), unique=False)
-    product_=db.Column(db.Integer)
-    quant_=db.Column(db.Integer)
+# class Data(db.Model):
+#     __tablename__="data"
+#     id =db.Column(db.Integer, primary_key=True)
+#     email_=db.Column(db.String(120), unique=False)
+#     product_=db.Column(db.Integer)
+#     quant_=db.Column(db.Integer)
 
-    def __init__(self, email_, product_, quant_):
-        self.email_=email_
-        self.product_=product_
-        self.quant_= quant_
+#     def __init__(self, email_, product_, quant_):
+#         self.email_=email_
+#         self.product_=product_
+#         self.quant_= quant_
 
 @app.route("/")   
 def index():
@@ -33,7 +33,7 @@ def success():
          email=request.form["email_name"]
          product=request.form["product_name"]
          quant=request.form["product_quantity"]
-         send_email(email, product, quant)
+         # send_email(email, product, quant)
          print(email, product, quant)
        
          #data=Data(email,product,quant)
