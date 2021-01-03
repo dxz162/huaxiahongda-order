513ce0aa1482f062c9d717326d7f0cedf90bf5b4
Zheng duanyang <you@example.com>
2021-01-02 19:34:00 -0500

    01/02/2021_8

--- app.py
+++ app.py
@@ -26,26 +26,6 @@ class Data(db.Model):
         self.quant_= quant_
 
 
-class send_email():
-    def __init__(self, email, product, quant):
-        from_email="davidzhengdy@gmail.com"
-        from_password="maomi2019"
-        to_email=email
-
-        subject="Product data"
-        message="您好,我是华夏矿产科技的桃小姐，您订购的产品规格为 <strong>%s</strong>. <br> 采购数量为 <strong>%s</strong>. <br> 感谢您的购买!" % (product, quant)
-
-        msg=MIMEText(message, 'html')
-        msg['Subject']=subject
-        msg['To']=to_email
-        msg['From']=from_email
-
-        gmail=smtplib.SMTP('smtp.gmail.com',587)
-        gmail.ehlo()
-        gmail.starttls()
-        gmail.login(from_email, from_password)
-        gmail.send_message(msg)
-
 
 
 @app.route("/")   
@@ -63,7 +43,7 @@ def success():
          email=request.form["email_name"]
          product=request.form["product_name"]
          quant=request.form["product_quantity"]
-         send_email(email, product, quant)
+         # send_email(email, product, quant)
          # print(email, product, quant)
        
          data=Data(email,product,quant)
